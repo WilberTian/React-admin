b48def1749d9d9fb52815f03cca325f98a4ae856
WilberTian <tianli917@hotmail.com>
2017-09-13 13:58:08 +0800

    add some grid examples in dashboard page

--- src/containers/dashboard/dashboardContainer.js
+++ src/containers/dashboard/dashboardContainer.js
@@ -1,10 +1,55 @@
 import React, { PureComponent } from 'react';
 
+import { Row, Col } from 'antd';
+
+import './dashboard-container.less';
+
 export default class DashboardContainer extends PureComponent {
     render() {
         return (
             <div className="dashboard-container">
-                dashboard-container
+                <Row gutter={16}>
+                    <Col span={6}>
+                        <div className="col-box h-180">col-box</div>
+                    </Col>
+                    <Col span={6}>
+                        <div className="col-box h-180">col-box</div>
+                    </Col>
+                    <Col span={6}>
+                        <div className="col-box h-180">col-box</div>
+                    </Col>
+                    <Col span={6}>
+                        <div className="col-box h-180">col-box</div>
+                    </Col>
+                </Row>
+
+                <Row gutter={16}>
+                    <Col span={18}>
+                        <div className="col-box h-180">col-box</div>
+                    </Col>
+                    <Col span={6}>
+                        <div className="col-box h-180">col-box</div>
+                    </Col>
+                </Row>
+
+                <Row gutter={16}>
+                    <Col span={12}>
+                        <div className="col-box h-180">col-box</div>
+                    </Col>
+                    <Col span={12}>
+                        <div className="col-box h-180">col-box</div>
+                    </Col>
+                </Row>
+
+                <Row gutter={16}>
+                    <Col span={18}>
+                        <div className="col-box h-240">col-box</div>
+                    </Col>
+                    <Col span={6}>
+                        <div className="col-box h-110">col-box</div>
+                        <div className="col-box h-110 mt-20">col-box</div>
+                    </Col>
+                </Row>
             </div>
         );
     }
